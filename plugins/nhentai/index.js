8319992b1de911c41121f607527e7c7a59b61d7a
CyferShepard <govenderthegan@gmail.com>
2022-05-14 13:11:14 +0200

    Update index.js
    Added support to search by url or ID

--- plugins/nhentai/index.js
+++ plugins/nhentai/index.js
@@ -10,6 +10,12 @@ const NHENTAI_IMAGE_URL = "https://i.nhentai.net/galleries/";
 
 
 function listChapters(url) {
+	
+	if(url.includes(NHENTAI_BASE_URL,0) ==false)
+	{
+		url=NHENTAI_BASE_URL+url+"/";
+	}
+	
 	var html = mango.get(url).body;
 
 	var urlMatch = /\/g\/([a-zA-Z0-9]+)\//.exec(url);
