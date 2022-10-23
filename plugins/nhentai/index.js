05c7a9c192a0c3d060d2c56ffd4b2c15f9353d21
Théo WEIMANN <theo.weimann.priv@gmail.com>
2022-10-23 18:41:49 +0200

    refactor: unify variable name & code cleanup
    Removes useless files and add them to the .gitignore
    Unify the variable naming scheme between functions
    Add space arround code blocks
    Remove unused code
    Add documentation

--- plugins/nhentai/index.js
+++ plugins/nhentai/index.js
@@ -1,38 +1,33 @@
-var pageURL;
-var pageCount = 0;
-var ended = false;
-var digits = 0;
-var imageType;
-var totalPages;
-
 // Nhentai IP address is mandatory to bypass the cloudflare protection (title API is protected by cloudflare)
-const NHENTAI_API_BASE_URL = 'http://138.2.77.198:3002/api/gallery/';
-const NHENTAI_API_SEARCH_URL = 'http://138.2.77.198:3002/api/galleries/search?query='
+const NHENTAI_INFO_API_BASE_URL = 'http://138.2.77.198:3002/api/gallery/';
+const NHENTAI_SEARCH_API_BASE_URL = 'http://138.2.77.198:3002/api/galleries/search?query='
 // Only image API that exposes cover
 const NHENTAI_COVER_SERVER_URL = 'https://t5.nhentai.net/galleries/';
 // API that exposes images
-const NHENTAI_IMAGE_SERVER_URL = "https://i.nhentai.net/galleries/";
+const NHENTAI_IMAGE_CDN_BASE_URL = "https://i.nhentai.net/galleries/";
 
+/*
+* Perform a search on nhentai by sauce ID and text query
+*/
 function searchManga(query) {
 
-	var mangas = []
+	const mangas = []
+	const regex = /\/g\/([a-zA-Z0-9]+)\//g
 
-	var regex = /\/g\/([a-zA-Z0-9]+)\//g
+	// Sauce ID query
 
-	var match = regex.exec(query)
+    var match = regex.exec(query)
 
-	// Multiple URL query
 	while (match != null){
 
-		var mangaID = match[1]
+		const mangaID = match[1]
 
-		var response = mango.get(NHENTAI_API_BASE_URL + mangaID.toString()).body;
+		const response = mango.get(NHENTAI_INFO_API_BASE_URL + mangaID.toString()).body;
+        const json = JSON.parse(response)
 
-		var json = JSON.parse(response)
-
-		var mangaTitle = json.title.english;
-		var mediaID = json.media_id
-		var coverType = convertType(json.images.cover.t)
+		const mangaTitle = json.title.english;
+		const mediaID = json.media_id
+		const coverType = convertType(json.images.cover.t)
 
 		if (mangaTitle){
 
@@ -47,14 +42,22 @@ function searchManga(query) {
 		match = regex.exec(query)
 
 	}
-	// String search
-	var response = mango.get(NHENTAI_API_SEARCH_URL + encodeURI(query)).body
-	var json = JSON.parse(response)
+
+	// Text query
+
+	const response = mango.get(NHENTAI_SEARCH_API_BASE_URL + encodeURI(query)).body
+	const json = JSON.parse(response)
+
 	if (!json.error){
-		var resultQuantity = json.result.length
+
+		const resultQuantity = json.result.length
+
 		for (var i = 0; i < resultQuantity; i+=1){
-			var result = json.result[i]
-			var coverType = convertType(result.images.cover.t)
+
+			const result = json.result[i]
+
+			const coverType = convertType(result.images.cover.t)
+
 			mangas.push({
 	                        id: result.id.toString(),
 	                        title: result.title.english,
@@ -62,55 +65,64 @@ function searchManga(query) {
                 	})
 		}
 	}
-	console.log(JSON.stringify(mangas))
 
 	return JSON.stringify(mangas)
 
 }
 
+/*
+* List all chapter given a sauce ID (should always return one chapter since it's how nhentai works)
+*/
 function listChapters(mangaID) {
 
-	const url = NHENTAI_API_BASE_URL + mangaID
+	const url = NHENTAI_INFO_API_BASE_URL + mangaID
 
 	if (!url.match(/\/api\/gallery\/[0-9]+$/))
 		mango.raise('Invalid API query url')
 
-	var response = mango.get(url).body;
+    const response = mango.get(url).body;
+	const json = JSON.parse(response);
 
-	var json = JSON.parse(response)
+	const chapterTitle = json.title.english;
+	const chapterLenght = json.images.pages.length;
 
-	var chapterTitle = json.title.english
-	var chapterLenght = json.images.pages.length
-
-	var chapters = [{
+	const chapters = [{
 		id: mangaID,
 		manga_title: 'nhentai',
 		title: chapterTitle,
 		pages: chapterLenght,
 		volume: null,
-                chapter: null,
-                groups: null,
-                language: null,
-                tags: []
+		chapter: null,
+		groups: null,
+		language: null,
+		tags: []
 	}];
 
 	return JSON.stringify(chapters);
 }
 
+/*
+* Used when a chapter is selected for download, preload download link in plugins storage and return chapter infos
+*/
 function selectChapter(id) {
-	var url = NHENTAI_API_BASE_URL + id;
-	var response = mango.get(url).body;
-	var json = JSON.parse(response)
 
-	var chapterTitle = json.title.english;
-	var mediaID = json.media_id
-	var pagesNumber = json.images.pages.length
+	const url = NHENTAI_INFO_API_BASE_URL + id;
 
-	var pageInfos = []
-	for (var i = 1; i <= pagesNumber; i = i + 1 ){
+	if (!url.match(/\/api\/gallery\/[0-9]+$/))
+        mango.raise('Invalid API query url')
 
-		var page = json.images.pages[0]
-		var pageType = convertType(page.t)
+	const response = mango.get(url).body;
+	const json = JSON.parse(response);
+
+	const chapterTitle = json.title.english;
+	const mediaID = json.media_id;
+	const chapterLenght = json.images.pages.length;
+
+	const pageInfos = [];
+	for (var i = 1; i <= chapterLenght; i = i + 1 ){
+
+		const page = json.images.pages[0];
+		const pageType = convertType(page.t);
 		pageInfos.push({mediaID: mediaID , index: i , pageType: pageType})
 
 	}
@@ -121,7 +133,7 @@ function selectChapter(id) {
 		id: id,
 		title: chapterTitle,
 		manga_title: chapterTitle,
-		pages: pagesNumber,
+		pages: chapterLenght,
 		volume: null,
 		chapter: null,
 		groups: null,
@@ -130,17 +142,20 @@ function selectChapter(id) {
 	});
 }
 
+/*
+* Navigate through the page url list and return url to mango
+*/
 function nextPage() {
 	var status = parseInt(mango.storage('status'))
-	var pageInfos = JSON.parse(mango.storage('pageInfos'))
+	const pageInfos = JSON.parse(mango.storage('pageInfos'));
 
 	if (status >= pageInfos.length) {
 		return JSON.stringify({});
 	}
 
-	var pageInfo = pageInfos[status]
-	var url = NHENTAI_IMAGE_SERVER_URL + pageInfo.mediaID + '/' + pageInfo.index + '.' + pageInfo.pageType
-	var filename = pageInfo.index + '.' + pageInfo.pageType
+	const pageInfo = pageInfos[status];
+	const url = NHENTAI_IMAGE_CDN_BASE_URL + pageInfo.mediaID + '/' + pageInfo.index + '.' + pageInfo.pageType;
+	const filename = pageInfo.index + '.' + pageInfo.pageType;
 
 	status = status + 1
         mango.storage('status', JSON.stringify( status ))
@@ -150,16 +165,10 @@ function nextPage() {
 		filename: filename
 	});
 }
-
-// https://stackoverflow.com/a/10073788
-function pad(n, width, z) {
-	z = z || '0';
-	n = n + '';
-	return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
-}
-
-
-// https://github.com/Zekfad/nhentai-api/blob/master/src/api.js
+/*
+* Convert nhentai API image type to file extensions
+* https://github.com/Zekfad/nhentai-api/blob/master/src/api.js
+*/
 function convertType(type) {
 	type = type.toLowerCase();
 	switch (type) {
